e64cd6dce859527efc375bada91ee28eccdb04c5
Jahanara Akter <jahanaraaktersuma100@gmail.com>
2024-02-28 18:47:09 +0600

    final

--- js/script.js
+++ js/script.js
@@ -29,14 +29,44 @@ for (const cartButtons of allCartButtons) {
       li.appendChild(p2);
       slectedContainer.appendChild(li);
 
-      const totalCost = document.getElementById('total-cost').innerText;
-      const convertedTotalCost = parseInt(totalCost);
-      document.getElementById('total-cost').innerText = convertedTotalCost + parseInt(PlacePrice);
+      const budget = document.getElementById('Total-budget').innerText;
+      const convertedBudget = parseInt(budget);
+      if (convertedBudget-parseInt(PlacePrice)<0){
+         alert('low budget earn more.')
+         return;
+      }
+      document.getElementById('Total-budget').innerText = convertedBudget - parseInt(PlacePrice);
+
+      SetTotal('total-cost', parseInt(PlacePrice));
+      SetgrandTotal('grand-total', parseInt(PlacePrice));
       setinnertext("home", count);
    });
 };
 //reuesable function start for counting
 function setinnertext(id, value) {
    document.getElementById(id).innerText = value;
+}
+
+function SetTotal(id, value) {
+   const totalCost = document.getElementById(id).innerText;
+   const convertedTotalCost = parseInt(totalCost);
+   const sum = convertedTotalCost + parseInt(value);
+   setinnertext('total-cost', sum);
+}
+
+function SetgrandTotal(category) {
+   const grandCost = document.getElementById('total-cost').innerText;
+   const convertedGrandTotalCost = parseInt(grandCost);
+   if (category == "bus") {
+      setinnertext('grand-total', convertedGrandTotalCost + 100);
+   } else if (category == "train") {
+      setinnertext('grand-total', convertedGrandTotalCost - 200);
+   }
+   else if (category == "plan") {
+      setinnertext('grand-total', convertedGrandTotalCost + 500);
+   }
+   else {
+      setinnertext('grand-total', convertedGrandTotalCost);
+   }
 }
 //reuesable function end for counting
